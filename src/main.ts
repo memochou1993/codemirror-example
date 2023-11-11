761147a19560f21b8b893f812e081799f43b6e3a
Memo Chou <memochou1993@gmail.com>
2023-11-12 01:43:23 +0800

    Add plugins

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,51 @@
 import './style.css';
-import { minimalSetup, EditorView } from 'codemirror';
+import { autocompletion, closeBrackets, closeBracketsKeymap, completionKeymap } from '@codemirror/autocomplete';
+import { defaultKeymap, history, historyKeymap } from '@codemirror/commands';
+import { javascript } from '@codemirror/lang-javascript';
+import { bracketMatching, defaultHighlightStyle, foldGutter, foldKeymap, indentOnInput, syntaxHighlighting } from '@codemirror/language';
+import { lintKeymap } from '@codemirror/lint';
+import { highlightSelectionMatches, searchKeymap } from '@codemirror/search';
+import { EditorState } from '@codemirror/state';
+import { crosshairCursor, drawSelection, dropCursor, EditorView, highlightActiveLine, highlightActiveLineGutter, highlightSpecialChars, keymap, lineNumbers, rectangularSelection } from '@codemirror/view';
 
-const initialText = 'console.log("hello, world")';
-
+const initialText = 'console.log("Hello, world!")';
 const targetElement = document.querySelector('#editor') as Element;
 
 new EditorView({
-  doc: initialText,
-  extensions: [
-    minimalSetup,
-  ],
   parent: targetElement,
+  state: EditorState.create({
+    doc: initialText,
+    extensions: [
+      lineNumbers(),
+      highlightActiveLineGutter(),
+      highlightSpecialChars(),
+      history(),
+      foldGutter(),
+      drawSelection(),
+      dropCursor(),
+      EditorState.allowMultipleSelections.of(true),
+      EditorView.theme({
+        '&': {},
+      }, { dark: true }),
+      indentOnInput(),
+      syntaxHighlighting(defaultHighlightStyle, { fallback: true }),
+      bracketMatching(),
+      closeBrackets(),
+      autocompletion(),
+      rectangularSelection(),
+      crosshairCursor(),
+      highlightActiveLine(),
+      highlightSelectionMatches(),
+      keymap.of([
+        ...closeBracketsKeymap,
+        ...defaultKeymap,
+        ...searchKeymap,
+        ...historyKeymap,
+        ...foldKeymap,
+        ...completionKeymap,
+        ...lintKeymap,
+      ]),
+      javascript(),
+    ],
+  }),
 });
